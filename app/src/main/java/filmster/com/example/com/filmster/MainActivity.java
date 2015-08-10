4c40f6eb0f5d9304bd2c35fc10396cc269272ebd
Terrell Green <trllgrn@gmail.com>
2015-08-10 15:59:41 -0700

    Added GridView layout; Loaded images from resources

--- app/src/main/java/filmster/com/example/com/filmster/MainActivity.java
+++ app/src/main/java/filmster/com/example/com/filmster/MainActivity.java
@@ -1,9 +1,19 @@
 package filmster.com.example.com.filmster;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Context;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,6 +21,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        GridView theGrid = (GridView) findViewById(R.id.gridview_main);
+        theGrid.setAdapter(new ImageAdapter(this));
+        theGrid.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(getBaseContext(),"Image " + position,Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
     @Override
@@ -33,5 +51,56 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+}
+
+class ImageAdapter extends BaseAdapter {
+    private Context mContext;
+    private ArrayList<Integer> mThumbs;
+
+
+    public ImageAdapter(Context c ){
+        mContext = c;
+        mThumbs = new ArrayList<Integer>();
+        mThumbs.add(R.drawable.sample_0);
+        mThumbs.add(R.drawable.sample_1);
+        mThumbs.add(R.drawable.sample_2);
+        mThumbs.add(R.drawable.sample_3);
+        mThumbs.add(R.drawable.sample_4);
+        mThumbs.add(R.drawable.sample_5);
+        mThumbs.add(R.drawable.sample_6);
+        mThumbs.add(R.drawable.sample_7);
+    }
+
+    @Override
+    public int getCount() {
+        return mThumbs.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ImageView imageView;
+        if (convertView == null) {
+            // if it's not recycled, initialize some attributes
+            imageView = new ImageView(mContext);
+            imageView.setLayoutParams(new GridView.LayoutParams(270,
+                                                                480));
+            imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+            imageView.setPadding(8, 8, 8, 8);
+        } else {
+            imageView = (ImageView) convertView;
+        }
+        imageView.setImageResource(mThumbs.get(position));
+        return imageView;
     }
 }
